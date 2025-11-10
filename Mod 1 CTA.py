7fe84e40c39fa61ecec820521ae6ed9b2111b81a
haley8726 <Lauren.morgan@csuglobal.edu>
2025-11-10 13:18:40 -0700

    Add files via upload

---
+++ Mod 1 CTA.py
@@ -0,0 +1,15 @@
+#Part 1
+
+num1 = int(input("enter a whole number:"))
+num2 = int(input("enter another whole number:"))
+result1 = num1 + num2
+result2 = num1 - num2
+print(f"added: {result1}, subtracted: {result2}")
+
+#Part 2
+
+num3 = int(input("enter a different whole number:"))
+num4 = int(input("enter another whole number:"))
+result3 = num3 * num4
+result4 = num3 / num4
+print(f"multiplied: {result3}, divided: {result4}")
